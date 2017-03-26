ae289b4144611f85057aa2bfa8e23d1e16bcfc4e
jshmcsh <1286196717@qq.com>
2017-03-26 10:34:23 +0800

    fifth commit

--- src/gittest.java
+++ src/gittest.java
@@ -1,6 +1,6 @@
 
 public class gittest {
 	public static void main(String[] args){
-		System.out.println("我正在测试中 jquery。。。。。hahahah");
+		System.out.println("我正在测试中 jquery。。。。。hahahah------------");
 	}
 }
